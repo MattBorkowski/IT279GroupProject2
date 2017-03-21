84b912b5dc3942e668017eea1203822c96210b9b
Adam Johnson <adam19325@gmail.com>
2017-03-21 18:47:52 -0500

    Added ShellSort files and made a CMake

---
+++ src/ShellSort.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Adam Johnson on 3/21/17.
+//
+
+#include "ShellSort.h"
---
+++ src/ShellSort.h
@@ -0,0 +1,14 @@
+//
+// Created by Adam Johnson on 3/21/17.
+//
+
+#ifndef UNTITLED_SHELLSORT_H
+#define UNTITLED_SHELLSORT_H
+
+
+class ShellSort {
+
+};
+
+
+#endif //UNTITLED_SHELLSORT_H
